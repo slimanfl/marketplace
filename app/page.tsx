d0532031282499cd2bf1b74e96a401d90126f59d
slimanfl <slimanite1996@gmail.com>
2023-12-22 08:01:15 -0800

    creat layout style and create max width wrapper components containes every component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import MaxWidthWrarpper from "@/components/MaxWidthWrarpper";
+
 export default function Home() {
-  return <div data-theme="light"></div>;
+  return (
+    <>
+      <MaxWidthWrarpper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl"></div>
+      </MaxWidthWrarpper>
+    </>
+  );
 }
